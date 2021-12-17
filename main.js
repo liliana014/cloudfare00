87d8f679145e6c882b7d079f327428c08c7a203c
liliana014 <93648399+liliana014@users.noreply.github.com>
2021-12-17 12:48:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,27 @@
 let res
   function shorturl() {
-
     if(document.querySelector("#text").value==""){
-	
         alert("Url cannot be empty!")
         return
     }
- 
 
     document.getElementById("searchbtn").disabled=true;
-    document.getElementById("searchbtn").innerHTML='<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Please wait...';
+	document.getElementById("searchbtn").innerHTML='<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Please wait...';
     fetch(window.location.pathname, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ url: document.querySelector("#text").value })
     }).then(function(response) {
- 
     return response.json();
   })
   .then(function(myJson) {
     res = myJson;
-        	      searchbtn.onclick = function(){
-	    result.select();
-	    document.execCommand('copy');
-		};     
     document.getElementById("searchbtn").disabled=false;
-   document.getElementById("searchbtn").innerHTML=' Shorten it';
-        
-        
+	document.getElementById("searchbtn").innerHTML=' Shorten it';
     if(res.key!=="")
     document.getElementById("result").value=window.location.host+res.key;
-   
     $('#exampleModal').modal('show')
-        
-    if(res.key!=="result")
-	    searchbtn.onclick = function(){
-	    result.select();
-	    document.execCommand('copy');
-	    window.location.reload();
-		
-	
-	     })
-        
-        
-        .catch(function(err){alert("Unknow error. Please retry!");
+  }).catch(function(err){alert("Unknow error. Please retry!");
   console.log(err);
   document.getElementById("searchbtn").disabled=false;
 	document.getElementById("searchbtn").innerHTML=' Shorten it';})
